d9e86c2175cea50dae0a6a696f8564a57f4c7def
Daria V <voronovadaria@gmail.com>
2021-06-05 21:11:32 +0300

    4.3.6. Math.abs [#373616]

---
+++ src/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+
+    private static boolean isValueValid(int value) {
+        return value >= 0 && value <= 7;
+    }
+
+    public static int way(int x1, int y1, int x2, int y2) {
+
+        if (isValueValid(x1)
+                && isValueValid(x2)
+                && isValueValid(y1)
+                && isValueValid(y2)
+                && x1 != x2 && y1 != y2) {
+
+            int xDifference = Math.abs(x2 - x1);
+            int yDifference = Math.abs(y2 - y1);
+
+            if (xDifference == yDifference) {
+                return xDifference;
+            }
+        }
+
+        return 0;
+    }
+}
